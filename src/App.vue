ec20cca02644bf4dee47396c769763049055aec4
pablo.morillas <pablo.morillas@est.fib.upc.edu>
2020-05-24 19:58:42 +0200

    Logout redirected

--- src/App.vue
+++ src/App.vue
@@ -69,6 +69,7 @@
                 localStorage.removeItem('googleId')
                 this.$forceUpdate()
                 this.componentKey += 1
+                this.$router.push("/contributions")
             },
             checkLogged() {
                 return 'userId' in localStorage
